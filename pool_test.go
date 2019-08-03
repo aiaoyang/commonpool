38eda3644fd44378b30f9c2df9f69043e50a25fe
yangjiangdong <yangjiangdong@tonnn.com>
2019-08-03 20:48:57 +0800

    add benchmark test

---
+++ pool_test.go
@@ -0,0 +1,33 @@
+package pool
+
+import (
+	"io"
+	"net"
+	"testing"
+)
+
+var (
+	factory = func() (io.Closer, error) {
+		return net.Dial("tcp", "127.0.0.1:10000")
+	}
+)
+
+func Benchmark_GetConn(b *testing.B) {
+	l, err := net.Listen("tcp", "127.0.0.1:10000")
+	if err != nil {
+		b.Errorf("tcp listener err: %s\n", err.Error())
+	}
+	defer l.Close()
+	P, err := NewPool(10, 100, factory)
+	if err != nil {
+		b.Errorf("create pool err: %s\n", err.Error())
+	}
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		c, _ := P.Get()
+		c.Close()
+	}
+	b.StopTimer()
+
+}
